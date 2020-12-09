bac0fcc7ed37574bbcce6509b74e16e5010f7b12
Wendy Shi <wendyshi@Wendys-MacBook-Air.local>
2020-12-09 02:26:13 -0500

    proj

--- P6/Project6/SDLProject/main.cpp
+++ P6/Project6/SDLProject/main.cpp
@@ -104,7 +104,7 @@ void ProcessInput() {
                         break;
                     case SDLK_RETURN:
                     if (currentScene == sceneList[0]){
-                        currentScene->state.nextScene = 1;
+                        currentScene->state.nextScene = 2;
                     }
                 }
                 break; // SDL_KEYDOWN
